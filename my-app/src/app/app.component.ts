87ed9ecd1bd5cfa7bb4dd7d22f9577a26174eafd
reymesson1 <reymesson1>
2020-05-26 03:06:48 -0400

    Working fine

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Router} from '@angular/router';
+import {RestapiService} from './restapi.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  constructor(public restapiservice : RestapiService, private router : Router) { }
+
+
 }
